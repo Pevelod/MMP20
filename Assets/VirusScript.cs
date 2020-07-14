f4cbba2f38657025797766694f9c6a5892d578a7
haschler <leonie@haschler.de>
2020-07-14 13:46:37 +0200

    Konsolen Kollision
    Konsolen Kollision

---
+++ Assets/VirusScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirusScript : MonoBehaviour
+{
+
+    void OnCollisionEnter2D (Collision2D colInfo)
+    {
+        Debug.Log(colInfo.relativeVelocity.magnitude);
+    }
+}
