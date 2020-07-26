7894fbe57e7ee7ea5d2a7921b044f21dae9b1f30
Pevelod <pevelod@web.de>
2020-07-27 01:22:26 +0200

    GameManager Planung
    Spritze hinzugefuegt

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    
+    public Pille pille;
+    public VirusScript virus;
+    
+    bool gamesEnded = false;
+
+    bool levelEnded = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Manipulate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manipulate : MonoBehaviour
+{
+    public void ReloadSCN()
+    {
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+            
+    }
+
+    public void SkipSCN()
+    {
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+            
+    }
+
+    public void EndSCN()
+    {
+        {
+            SceneManager.LoadScene(0);
+        }
+
+            
+    }
+    
+}
