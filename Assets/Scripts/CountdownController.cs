cc0ccdfab2793f269526ffadae827511fe20af9a
Pevelod <pevelod@web.de>
2020-07-27 03:54:54 +0200

    Buttons funktionieren
    Navigation in Leveln moeglich

---
+++ Assets/Scripts/CountdownController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownController : MonoBehaviour
+{
+    public float totalTime;
+    public Text text;
+    private float seconds;
+
+
+    public void update()
+    {
+        totalTime -= Time.deltaTime;
+
+        seconds = (int)(totalTime % 60);
+
+        text.text = seconds.ToString();
+    }
+}
