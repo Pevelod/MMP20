a85ae43a8fda459dfb532f075af6713b120ab931
Pevelod <pevelod@web.de>
2020-07-28 04:14:13 +0200

    Lauffaehig #9
    Endcarf funktioniert noch nicht, sonst ganz ok
    
    Behelfsweise Impressum Button auf Startmenu

--- Assets/Scripts/GameButtons.cs
+++ Assets/Scripts/GameButtons.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class GameButtons : MonoBehaviour
 {
+
+    int scenes = 3;
     public void Play_Button()
     {
         SceneManager.LoadScene(1);
@@ -18,6 +20,12 @@ public class GameButtons : MonoBehaviour
     public void Weiter_Button()
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+    public void Impressum_Button()
+        {
+            Debug.Log("Impressum gedrueckt");
+            SceneManager.LoadScene(7);
         }
 
     public void Quit_Button()
